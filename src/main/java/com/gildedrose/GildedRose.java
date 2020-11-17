c99df561515470f2c3a0b53c92a8dfb928574bcd
Tauras <tauras@satalia.com>
2020-11-17 20:55:15 +0200

    refactors regular for loop to a for each

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,9 +8,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item item = items[i];
-
+        for (Item item : items) {
             if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
                 continue;
@@ -21,27 +19,27 @@ class GildedRose {
                 continue;
             }
 
-            if (!items[i].name.equals("Aged Brie")) {
-                if (items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1;
+            if (!item.name.equals("Aged Brie")) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
                 }
             }
 
-            items[i].sellIn = items[i].sellIn - 1;
+            item.sellIn = item.sellIn - 1;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
 
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
