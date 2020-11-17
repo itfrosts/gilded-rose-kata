995dbb348899cd6093eeb928d58bf15884287a9a
Tauras <tauras@satalia.com>
2020-11-17 20:54:12 +0200

    refactor product update for backstage passes

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,28 +16,18 @@ class GildedRose {
                 continue;
             }
 
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                updateConcertTicket(item);
+                continue;
+            }
+
+            if (!items[i].name.equals("Aged Brie")) {
                 if (items[i].quality > 0) {
                     items[i].quality = items[i].quality - 1;
                 }
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
                 }
             }
 
@@ -45,13 +35,10 @@ class GildedRose {
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    } else {
-                        items[i].quality = 0;
+                    if (items[i].quality > 0) {
+                        items[i].quality = items[i].quality - 1;
                     }
+
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
@@ -59,5 +46,30 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    private void updateConcertTicket(Item ticket) {
+        if (ticket.sellIn <= 0) {
+            ticket.sellIn -= 1;
+            ticket.quality = 0; // ticket quality drops to zero after the concert
+            return;
+        }
+
+        // Ticket quality increases 3x when less than 5
+        if (ticket.sellIn <= 5) {
+            ticket.sellIn -= 1;
+            ticket.quality = Math.min(ticket.quality + 3, 50);
+            return;
+        }
+
+        // Ticket quality increases twice when less than 10 days
+        if (ticket.sellIn <= 10) {
+            ticket.sellIn -= 1;
+            ticket.quality = Math.min(ticket.quality + 2, 50);
+            return;
+        }
+
+        ticket.sellIn -= -1;
+        ticket.quality = Math.min(ticket.quality + 1, 50);
     }
 }
