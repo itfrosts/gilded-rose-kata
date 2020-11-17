2032b9f4aaaa05304037de3d7c23f0d03228974d
Tauras <tauras@satalia.com>
2020-11-17 21:58:40 +0200

    refactors how concert ticket values are updated

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -36,27 +36,19 @@ class GildedRose {
     }
 
     private void updateConcertTicket(Item ticket) {
-        if (ticket.sellIn <= 0) {
-            ticket.sellIn -= 1;
+        ticket.sellIn -= 1;
+        
+        if (ticket.sellIn < 0) {
             ticket.quality = 0; // ticket quality drops to zero after the concert
             return;
         }
-
-        // Ticket quality increases 3x when less than 5
-        if (ticket.sellIn <= 5) {
-            ticket.sellIn -= 1;
-            ticket.quality = Math.min(ticket.quality + 3, 50);
-            return;
-        }
-
+    
         // Ticket quality increases twice when less than 10 days
-        if (ticket.sellIn <= 10) {
-            ticket.sellIn -= 1;
-            ticket.quality = Math.min(ticket.quality + 2, 50);
-            return;
+        if (ticket.sellIn < 10) {
+            // Ticket quality increases by an extra 1 when less than 5 days left
+            ticket.quality += ticket.sellIn < 5 ? 2 : 1;
         }
 
-        ticket.sellIn -= -1;
         ticket.quality = Math.min(ticket.quality + 1, 50);
     }
 }
